05dd3f2f5d784dc120de71d1532ff1ef4adbeccf
Harshal Mahajan <hfm5238@psu.edu>
2020-08-27 13:58:56 +0000

    .

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = input("Enter your name:")
-print(f"Hello , {name}!")
+name = input("Enter your name: ")
+print("Hello, " + str(name) + "!")
+
 age = input("Enter your age: ")
 age = int(age)
-print(f"You will be {age+1} next year")
+print("You will be " + str(age+1) + " next year.")
